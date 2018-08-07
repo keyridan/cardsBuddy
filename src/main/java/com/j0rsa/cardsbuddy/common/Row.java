80579933146a8a85dba2a692f2871392176d3b6c
keyridan <keyridan@gmail.com>
2018-08-07 19:24:42 +0200

    added type field

--- src/main/java/com/j0rsa/cardsbuddy/common/Row.java
+++ src/main/java/com/j0rsa/cardsbuddy/common/Row.java
@@ -1,10 +1,18 @@
 package com.j0rsa.cardsbuddy.common;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = Info.class, name = "INFO"),
+        @JsonSubTypes.Type(value = SmallTitleRow.class, name = "SMALL_TITLE"),
+        @JsonSubTypes.Type(value = TitleRow.class, name = "TITLE"),
+})
 public abstract class Row {
     private String value;
 }
